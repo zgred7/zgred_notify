1d7918fcbe5d81f7582475240c61fa273d17c60b
zgred <93154804+zgred7@users.noreply.github.com>
2024-12-22 10:08:10 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,5 +1,5 @@
 exports('sendNotify', function(data) SendNUIMessage({action = 'Notify', desc = data}) end)
 
 RegisterCommand('testNotify', function()
-    exports['zgred_notify']:sendNotify('Otworzyłeś panel Administracyjny')
+    exports['zgred_notify']:sendNotify('Testowe powiadomienie')
 end)
